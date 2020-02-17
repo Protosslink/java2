4fe2a4abdcbccc8d123f6bb8ce8c32341d2088c3
ProtossLINK <LINK-21@yandex.ru>
2020-02-17 16:41:21 +0300

    Задание 4 Массив

--- src/main/java/Task4Arr.java
+++ src/main/java/Task4Arr.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Task4Arr {
 
     //Генерация случайного числа в диапозоне min, max
@@ -6,8 +8,10 @@ public class Task4Arr {
         return (int) (Math.random() * diapason) - max;
     }
 
-
     public void addArrInt() {
+
+        int maxNegativeNum = 0; //Максимальный отрицательный элемент
+        int minPositiveNum = 0; //Минимальный положительный элемент
         //Объявление массива
         int[] arr;
         //Определение Массива
@@ -15,9 +19,29 @@ public class Task4Arr {
         //Заполнение массива
         for (int i = 0; i < arr.length - 1; i++) {
             arr[i] = numArr(-10, 10);
-            //System.out.println(arr[i]);
         }
+        //Вывод массива на экран
+        System.out.println(Arrays.toString(arr));
+
+        //Нахождение максимального отрицательного элемента
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < maxNegativeNum) {
+                maxNegativeNum = arr[i];
+            }
+        }
+        System.out.println("====================================================================");
+        System.out.println("Максимальный отрицательный элемет = " + maxNegativeNum);
+
+        //Нахождение минимального положительного числа
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0 && arr[i] < arr[i + 1]) {
+                minPositiveNum = arr[i];
+            }
+        }
+        System.out.println("Минимальный положительный элемент = " + minPositiveNum);
     }
+
 }
 
 
