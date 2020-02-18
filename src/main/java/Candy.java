beceae372a8df45b61fe97f60646ba759c197afb
ProtossLINK <LINK-21@yandex.ru>
2020-02-18 10:52:31 +0300

    Задание 4 Массив готово Задание запускается из класса Base

---
+++ src/main/java/Candy.java
@@ -0,0 +1,44 @@
+public class Candy {
+
+    private String title;
+    private int weight;
+    private double price;
+    private String color;
+
+    //Медоты названия конфеты//
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle() {
+        this.title = title;
+    }
+
+    //Методы веса конфеты//
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight() {
+        this.weight = weight;
+    }
+
+    //Методы цены конфеты//
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice() {
+        this.price = price;
+    }
+
+    //Методы цвета конфеты//
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor() {
+        this.color = color;
+    }
+
+}
