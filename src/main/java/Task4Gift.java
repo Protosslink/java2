65f5095a0034c26430271ccdcf7915b4db9f7cee
ProtossLINK <LINK-21@yandex.ru>
2020-02-18 16:12:46 +0300

    Задание 5 подарок Задание запускается из класса Base

--- src/main/java/Task4Gift.java
+++ src/main/java/Task4Gift.java
@@ -1,38 +1,51 @@
 public class Task4Gift {
 
-    Candy redCandy = new Candy("Аленка", 40, 50.50,"красный");
+    Candy redCandy = new Candy("Аленка", 40, 50.50, "красный");
 
     //Candy, Jellybean, etc.
 
-    class Candy{
+    class Candy {
+
+        //Переменные класса Candy
         private String title;
         private int weight;
         private double price;
         private String color;
 
-        public Candy(String title, int weight, double price, String color){
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
             this.title = title;
-            this. weight = weight;
-            this. price = price;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
             this.color = color;
-            System.out.println("Название " + title + ", Вес " + weight + ", Цена " + price + ", Цвет " + color);
         }
 
     }
-
-    public void addCandy() {
-    }
-
-    public void addJellyBean(){
-
-    }
-    public void addEtc(){
-
-    }
-
-
-
-    }
+}
 
 
 
