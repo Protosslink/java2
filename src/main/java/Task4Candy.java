6dc8badad0c787fd878d015cac860eb24a2201cc
ProtossLINK <LINK-21@yandex.ru>
2020-02-27 11:19:47 +0300

    Задание 4 подарок Задание запускается из класса Base

--- src/main/java/Task4Candy.java
+++ src/main/java/Task4Candy.java
@@ -12,26 +12,35 @@ public class Task4Candy {
         Jellybean jellybean = new Jellybean();
         jellybean.setTitle("Мармелад");
         jellybean.setWeight(20);
-        jellybean.setPrice(100);
+        jellybean.setPrice(75.50);
         //Создание объекта Зефир
         Zephyr zephyr = new Zephyr();
         zephyr.setTitle("Зефир");
         zephyr.setWeight(30);
-        zephyr.setPrice(200);
+        zephyr.setPrice(200.12);
         //Создание объекта Шоколад
         Chocolate chocolate = new Chocolate();
         chocolate.setTitle("Шоколад");
         chocolate.setWeight(40);
-        chocolate.setPrice(300);
+        chocolate.setPrice(300.30);
         //Информация о сладостях
         jellybean.showInformationAboutCandy();
         chocolate.showInformationAboutCandy();
         zephyr.showInformationAboutCandy();
         //Подсчет общей цены подарка
-        System.out.println("Стоимость подарка = " + jellybean.getPrice() + chocolate.getPrice() + zephyr.getPrice());
+        sumPrice(jellybean.getPrice(), chocolate.getPrice(), zephyr.getPrice());
         //Подсчет Общего веса подарка
-        System.out.println("Вес подарка = " + jellybean.getWeight() + chocolate.getWeight() + zephyr.getWeight());
+        int sumWeight = jellybean.getWeight() + chocolate.getWeight() + zephyr.getWeight();
+        System.out.println("Вес подарка = " + sumWeight);
 
+    }
+    public void sumPrice(double a, double b, double c){
+        double d = a + b + c;
+        System.out.println("Стоимость подарка = " + d);
+    }
+    public void sumWeight(int a, int b, int c){
+        int d = a + b + c;
+        System.out.println("Вес подарка = " + d);
     }
 }
 
