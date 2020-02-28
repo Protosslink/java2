6f2b5d5fae334248877a2ea56f7ab6defb045dfa
ProtossLINK <LINK-21@yandex.ru>
2020-02-28 11:00:59 +0300

    Задание 8

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -5,21 +5,21 @@ public class CalcTest {
     @Test
     public void CheckNumOneDefault(){
         Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
-        Assert.assertEquals("Значение первого числа должно ровняться нулю", 0, task5CalcOpp1.getNumOne(), 0);
+        Assert.assertEquals("Значение по умолчанию первого числа должно ровняться нулю", 0, task5CalcOpp1.getNumOne(), 0);
     }
     @Test
     public void CheckNumTwoDefault(){
         Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
-        Assert.assertEquals("Значение первого числа должно ровняться нулю", 0, task5CalcOpp1.getNumTwo(), 0);
+        Assert.assertEquals("Значение по умолчанию второго числа должно ровняться нулю", 0, task5CalcOpp1.getNumTwo(), 0);
     }
     @Test
     public void CheckNumResultDefault(){
         Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
-        Assert.assertEquals("Значение первого числа должно ровняться нулю", 0, task5CalcOpp1.getNumResult(), 0);
+        Assert.assertEquals("Значение по умолчанию результата должно ровняться нулю", 0, task5CalcOpp1.getNumResult(), 0);
     }
     @Test
     public void CheckNumChoiceOperationDefault(){
         Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
-        Assert.assertEquals("Значение первого числа должно ровняться нулю", 0, task5CalcOpp1.getNumChoiceOperation(), 0);
+        Assert.assertEquals("Значение по умолчанию выбора операции должно ровняться нулю", 0, task5CalcOpp1.getNumChoiceOperation(), 0);
     }
 }
