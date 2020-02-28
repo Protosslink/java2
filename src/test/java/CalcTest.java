f373f1e518b3423144202784aa112efaac3878ca
ProtossLINK <LINK-21@yandex.ru>
2020-02-28 10:58:48 +0300

    Задание Задание 8 UnitTest Задание запускается из класса Base

---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,25 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalcTest {
+    @Test
+    public void CheckNumOneDefault(){
+        Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
+        Assert.assertEquals("Значение первого числа должно ровняться нулю", 0, task5CalcOpp1.getNumOne(), 0);
+    }
+    @Test
+    public void CheckNumTwoDefault(){
+        Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
+        Assert.assertEquals("Значение первого числа должно ровняться нулю", 0, task5CalcOpp1.getNumTwo(), 0);
+    }
+    @Test
+    public void CheckNumResultDefault(){
+        Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
+        Assert.assertEquals("Значение первого числа должно ровняться нулю", 0, task5CalcOpp1.getNumResult(), 0);
+    }
+    @Test
+    public void CheckNumChoiceOperationDefault(){
+        Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
+        Assert.assertEquals("Значение первого числа должно ровняться нулю", 0, task5CalcOpp1.getNumChoiceOperation(), 0);
+    }
+}
