c25b3820968a39740953ce75e62f7e4fd5a2add1
ProtossLINK <LINK-21@yandex.ru>
2020-03-04 15:50:02 +0300

    Задание 8

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -3,23 +3,23 @@ import org.junit.Test;
 
 public class CalcTest {
     @Test
-    public void CheckNumOneDefault(){
+    public void checkNumOneDefault(){
         Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
         Assert.assertEquals("Значение по умолчанию первого числа должно ровняться нулю", 0, task5CalcOpp1.getNumOne(), 0);
     }
     @Test
-    public void CheckNumTwoDefault(){
+    public void checkNumTwoDefault(){
         Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
         Assert.assertEquals("Значение по умолчанию второго числа должно ровняться нулю", 0, task5CalcOpp1.getNumTwo(), 0);
     }
     @Test
-    public void CheckNumResultDefault(){
+    public void checkNumResultDefault(){
         Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
         Assert.assertEquals("Значение по умолчанию результата должно ровняться нулю", 0, task5CalcOpp1.getNumResult(), 0);
     }
     @Test
-    public void CheckNumChoiceOperationDefault(){
+    public void checkNumChoiceOperationDefault(){
         Task5CalcOpp task5CalcOpp1 = new Task5CalcOpp();
         Assert.assertEquals("Значение по умолчанию выбора операции должно ровняться нулю", 0, task5CalcOpp1.getNumChoiceOperation(), 0);
     }
-}
+   }
